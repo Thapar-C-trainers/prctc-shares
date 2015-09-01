d484212a07dd7801ee2992fe9ff69aa648a84b80
Aditya-Tolikar <adityatolikar@gmail.com>
2015-09-01 17:12:25 +0530

    Pointer usage as array writing controller.
    Usage of pointer to write on array using external function.

---
+++ direct-arr-point.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void fun(int A[],int n);
+
+int main() {
+int i,j,temp,A[] = {9,9,8,5,5},n = 5;
+fun(A,n);
+for(i=0;i<n;i++) printf("\n%d",A[i]);
+printf("\n");
+return 0;
+}
+
+void fun(int *A,int n){
+int i;
+for(i=0;i<n;i++) A[i]*=10;
+}
