3d42da65225fcff534a4ed42cedeb1645e960556
Aditya-Tolikar <adityatolikar@gmail.com>
2015-09-01 18:36:06 +0530

    Update DescendArr.cpp

--- DescendArr.cpp
+++ DescendArr.cpp
@@ -1,56 +1,61 @@
 #include <iostream.h>
 
+int *DecrOrderIN(int ssp[] , const int length);
+int getMaxIN(int out[], int length, int count, int changed);
+#define length 20
 main()
 {
-	int *DecrOrderIN(int ssp[],const int length);
-	int getMaxIN(int ssp[], int length);
-	
 	const int size=20;
 	int inp[size] ;
 	int *ordered;
-	
+
 	cout<<"Array nums\n";
-	
+
 	for (int n=0;n<size;++n)
 	{
 		//input loop...
 		cout<<"\n"<<n<<"th number\t";
 		cin>>inp[n];
 	}
-	
-	ordered = DecrOrderIN(inp , size);
+
+	*ordered = *DecrOrderIN(inp , size);
 
 	for (n=0;n<size;++n)
 	{
 		//output loop...
 		cout<<*ordered+n<<"\n";
-	}	
-	
+	}
+
 return 0;
 }
 
 int *DecrOrderIN(int ssp[],const int length)
 {
-	int getMaxIN(int ssp[] , int length);
 	static int out[length];
-	
-	for (int count=0; count < length; ++count)
+
+	//Cloning the array to static one...
+	for(int count=0; count < length; ++count) {out[count]=ssp[count];}
+
+	int temp, changed;
+	for (count=0; count < length; ++count)
 	{
-		out[count] = getMaxIN(ssp , length);
+		temp = out[count];
+		out[count] = getMaxIN(out, length,count,changed);
+		out[changed] = temp;
 	}
 	//The output is supposed to be returned to a pointer.
 	return out;
 }
 
-int getMaxIN(int ssp[], int length)
+int getMaxIN(int ssp[], int length, int starter, int *tag)
 {
-	int temp;
-	temp = ssp[1];
 
-	for (int count=0; count < length ; ++count)
+	for (int temp = ssp[starter]; starter < (length-1) ; ++starter)
 	{
-		if(temp < ssp[count]) {temp = ssp[count];}
+		if(temp < ssp[starter+1]) {temp = ssp[starter+1]; *tag = (starter+1);}
+
+		else continue ;
 	}
-	
+
 	return temp;
 }
